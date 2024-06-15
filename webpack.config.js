443258f5bb201c12a8ee9fb6283b98dc715e9b19
Jason Huang <jason.hts0817@gmail.com>
2024-06-15 11:40:06 +0800

    feat: modify webpack setting

--- webpack.config.js
+++ webpack.config.js
@@ -4,30 +4,33 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
 
 const isProduction = process.env.NODE_ENV == "production";
 
 const config = {
   entry: {
-    main: "./src/index.js",
     coolhealth: "./src/scss/coolhealth.scss",
-    'coolhealth-rwd': "./src/scss/coolhealth-rwd.scss",
-    'coolhealth-sidebar': "./src/scss/coolhealth-sidebar.scss",
-    'coolhealth-color-type': "./src/scss/coolhealth-color-type.scss",
+    "coolhealth-rwd": "./src/scss/coolhealth-rwd.scss",
+    "coolhealth-sidebar": "./src/scss/coolhealth-sidebar.scss",
+    "coolhealth-color-type": "./src/scss/coolhealth-color-type.scss",
   },
   optimization: {
     removeEmptyChunks: true,
   },
   output: {
     clean: true,
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
   devServer: {
-    host: "localhost"
+    host: "localhost",
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: "index.html", minify: false, inject: !isProduction }),
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      minify: false,
+      inject: !isProduction,
+    }),
     new MiniCssExtractPlugin({ filename: "css/[name].css" }),
     new RemoveEmptyScriptsPlugin(),
     new CopyPlugin({
@@ -36,8 +39,8 @@ const config = {
         { from: "src/fonts", to: "./fonts" },
         { from: "src/images", to: "./images" },
         { from: "src/js", to: "./js" },
-      ]
-    })
+      ],
+    }),
   ],
   module: {
     rules: [
@@ -47,12 +50,15 @@ const config = {
           { loader: MiniCssExtractPlugin.loader },
           { loader: "css-loader", options: { url: false } },
           { loader: "postcss-loader" },
-          { loader: 'sass-loader', options: { sassOptions: { outputStyle: "expanded" } }, }
+          {
+            loader: "sass-loader",
+            options: { sassOptions: { outputStyle: "expanded" } },
+          },
         ],
-      }
+      },
     ],
   },
-  target: 'web'
+  target: "web",
 };
 
 module.exports = () => {
